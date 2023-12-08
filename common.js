8a9049918de0be9f6257ec4d2bc0a41718ff59ba
babajide234 <babajide234@gmail.com>
2023-12-08 08:28:04 +0100

    initial commit

---
+++ common.js
@@ -0,0 +1,95 @@
+ const menu =[
+    {
+        header: "general"
+    },
+    {
+        name: 'home',
+        url:'/',
+        icon:"home" 
+    },
+    {
+        name: 'recently updated',
+        url:'/',
+        icon:"x" 
+    },
+    {
+        name: 'categories',
+        url:'/',
+        icon:"PiFolderThin" 
+    },
+    {
+        name: 'FAC',
+        url:'/',
+        icon:"BsChatSquare" 
+    },
+    {
+        header: "personal"
+    },
+    {
+        name: 'bookmarks',
+        icon:"BsBookmark",
+        children:[
+            {
+                name:'bookmark Child',
+                url:"",
+                icon:""
+            }
+        ]
+    },
+    {
+        name: 'update request',
+        url:'/',
+        icon:"TbLocation" 
+    },
+    {
+        name: 'settings',
+        url:'/',
+        icon:"IoSettingsOutline" 
+    },
+]
+
+
+const contentData ={
+    "Result": [
+        {
+            "ContentId": 6,
+            "RefNo": "11f61540-c1d2-4681-8f50-105d57848cfc",
+            "Title": "Insurance claims management - General Insurance",
+            "Description": "General Insurance claims management",
+            "SubsidiaryId": 2,
+            "Uploadeby": "fruser@nsia.com.ng",
+            "Created": "2023-10-29T16:03:31.95",
+            "Modifiedby": "sharepoint@nsia.com.ng",
+            "Modified": "2023-10-29T16:03:37.567",
+            "Classification": "Private",
+            "Status": "Published",
+            "Archive": false,
+            "ContentBody": "<p>Background:- The document is for management of claims for general business</p><p>Main Information:- claims information </p>",
+            "ContentType": "Document",
+            "DeptId": 1,
+            "DelayPublication": true,
+            "PublicationDate": "2023-10-30T00:00:00"
+        },
+        {
+            "ContentId": 7,
+            "RefNo": "030d389b-64a1-41c2-a1e7-684a73abe634",
+            "Title": "General Insurance Importance",
+            "Description": "Importance of General Insurance claims management",
+            "SubsidiaryId": 2,
+            "Uploadeby": "fruser@nsia.com.ng",
+            "Created": "2023-10-29T16:17:35.037",
+            "Modifiedby": "sharepoint@nsia.com.ng",
+            "Modified": "2023-10-29T16:18:35.147",
+            "Classification": "Private",
+            "Status": "Published",
+            "Archive": false,
+            "ContentBody": "<p>Background:- The document is for management of claims for general business</p><p>Main Information:- claims information </p>",
+            "ContentType": "Document",
+            "DeptId": 1,
+            "DelayPublication": true,
+            "PublicationDate": "2023-10-30T00:00:00"
+        }
+    ],
+    "ResponseCode": 100
+}
+// export { menu };
---
+++ index.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+        let currentSlide = 0;
+        updateDots(currentSlide)
+
+        function showSlide(index) {
+            console.log(index)
+            $('.slide').removeClass('hidden').addClass('hidden');
+            $('.slide').eq(index).removeClass('hidden');
+            updateDots(index);
+
+        }
+
+        function updateDots(index) {
+            $('.dot').removeClass('bg-gray-100 bg-gray-100/40');
+            $('.dot').not(':eq(' + index + ')').addClass('bg-gray-100/40');
+            $('.dot').eq(index).addClass('bg-gray-100');
+        }
+
+
+        function nextSlide() {
+            currentSlide = (currentSlide + 1) % $('.slide').length;
+            showSlide(currentSlide);
+        }
+
+        function prevSlide() {
+            currentSlide = (currentSlide - 1 + $('.slide').length) % $('.slide').length;
+            showSlide(currentSlide);
+        }
+
+        $('.dot').click(function () {
+            const dotIndex = $(this).index();
+            showSlide(dotIndex);
+        });
+
+        // Handle arrow key navigation
+        $(document).keydown(function (e) {
+            if (e.keyCode === 37) {
+                prevSlide();
+            } else if (e.keyCode === 39) {
+                nextSlide();
+            }
+        });
+
+        // Handle swipe gestures for touch devices
+        let startX;
+        let endX;
+        $('#slider').on('touchstart', function (e) {
+            startX = e.touches[0].pageX;
+        });
+
+        $('#slider').on('touchmove', function (e) {
+            endX = e.touches[0].pageX;
+        });
+
+        $('#slider').on('touchend', function () {
+            const deltaX = endX - startX;
+            if (deltaX > 50) {
+                prevSlide();
+            } else if (deltaX < -50) {
+                nextSlide();
+            }
+        });
+
+
+        
+});
